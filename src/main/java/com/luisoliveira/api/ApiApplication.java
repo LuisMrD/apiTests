1d782155d3e4e80ac6a2f5c5dccb62c039bcdea3
LuisMrD <luisfelipemo@gmail.com>
2022-01-16 15:58:08 -0300

    created User entity class

--- src/main/java/com/luisoliveira/api/ApiApplication.java
+++ src/main/java/com/luisoliveira/api/ApiApplication.java
@@ -1,5 +1,6 @@
 package com.luisoliveira.api;
 
+import com.luisoliveira.api.domain.User;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ApiApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
---
+++ src/main/java/com/luisoliveira/api/domain/User.java
@@ -0,0 +1,24 @@
+package com.luisoliveira.api.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+    private String password;
+}
